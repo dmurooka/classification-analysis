3c519dfbe36c1931fa5eedbe2c90b61018611690
Daishin Murooka <dmurooka@gmail.com>
2024-04-29 17:10:13 +0900

    Demonstrate knearest neighbor classification using python libraries

---
+++ knearest-neighbors-classification.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+import seaborn as sns
+from sklearn import datasets
+
+
+iris = datasets.load_iris()
+
+df = pd.DataFrame(
+    {
+        "Sepal length": iris.data[:, 0],
+        "Sepal width": iris.data[:, 1],
+        "Petal length": iris.data[:, 2],
+        "Petal width": iris.data[:, 3],
+        "Species": iris.target,
+    }
+)
+
+print(df.head())
+
+# Replaces the original DataFrame with the new one that excludes row where "Species" is not 0
+df = df[df["Species"] != 0]
+
+print(df.info)
