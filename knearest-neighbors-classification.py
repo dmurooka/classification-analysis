5ceaedaa36eb10ec60d1fcf305d7cb9f0a3d8745
Daishin Murooka <dmurooka@gmail.com>
2024-05-03 15:31:28 +0900

    Demonstrate training

--- knearest-neighbors-classification.py
+++ knearest-neighbors-classification.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import datasets
+from sklearn.model_selection import train_test_split
 
 
 iris = datasets.load_iris()
@@ -17,9 +18,18 @@ df = pd.DataFrame(
     }
 )
 
-print(df.head())
+# print(df.head())
 
 # Replaces the original DataFrame with the new one that excludes row where "Species" is not 0
 df = df[df["Species"] != 0]
 
-print(df.head)
+# print(df.head)
+
+sns.relplot(data=df, x="Sepal length", y="Sepal width", hue="Species")
+
+# Train-Test Split
+X = df[df.columns[:2]]
+y = df[df.columns[-1]]
+
+X_train, X_test, Y_train, y_test = train_test_split(X, y, test_size=0.5)
+print(X_train[:5])
