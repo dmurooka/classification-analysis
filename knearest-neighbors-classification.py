50a7ea1f00f1d6ed2124b1eaa4fc7cda5021886c
Daishin Murooka <dmurooka@gmail.com>
2024-05-04 16:09:24 +0900

    Train a model

--- knearest-neighbors-classification.py
+++ knearest-neighbors-classification.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
 
 iris = datasets.load_iris()
@@ -33,3 +36,19 @@ y = df[df.columns[-1]]
 
 X_train, X_test, Y_train, y_test = train_test_split(X, y, test_size=0.5)
 print(X_train[:5])
+
+# Scaling
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+print(X_train[:5])
+
+# Train a model
+k = 1
+
+classifier = KNeighborsClassifier(n_neighbors=k)
+classifier.fit(X_train, Y_train)
+
+y_pred = classifier.predict(X_test)
