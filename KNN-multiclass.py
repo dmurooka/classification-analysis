cf1b26ded781538f094888e4302e6fe13599a37d
Daishin Murooka <dmurooka@gmail.com>
2024-05-10 09:10:37 +0900

    Implement KNN multiclass

--- KNN-multiclass.py
+++ KNN-multiclass.py
@@ -60,3 +60,44 @@ print(result1)
 
 result2 = accuracy_score(y_test, y_pred)
 print("Accuract", result2)
+
+# Best K
+
+
+def knn_tuning(k):
+    classifier = KNeighborsClassifier(n_neighbors=k)
+    classifier.fit(X_train, y_train)
+    y_pred = classifier.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    return accuracy
+
+
+print("KNN Tuning", knn_tuning(1))
+print("KNN Tuning", knn_tuning(5))
+
+knn_results = pd.DataFrame({"K": np.arange(1, len(X_train), 5)})
+
+knn_results["Accuracy"] = knn_results["K"].apply(knn_tuning)
+print(knn_results)
+
+
+# Optimize weights
+def knn_tuning_uniform(k):
+    classifier = KNeighborsClassifier(n_neighbors=k, weights="uniform")
+    classifier.fit(X_train, y_train)
+    y_pred = classifier.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    return accuracy
+
+
+def knn_tuning_distance(k):
+    classifier = KNeighborsClassifier(n_neighbors=k, weights="distance")
+    classifier.fit(X_train, y_train)
+    y_pred = classifier.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    return accuracy
+
+
+knn_results["Uniform"] = knn_results["K"].apply(knn_tuning_uniform)
+knn_results["Distance"] = knn_results["K"].apply(knn_tuning_distance)
+print(knn_results)
