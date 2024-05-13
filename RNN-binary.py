7c2140ef7cbe287b88966ba240203ea1718a9872
Daishin Murooka <dmurooka@gmail.com>
2024-05-13 10:59:51 +0900

    Optimize weights

--- RNN-binary.py
+++ RNN-binary.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
 
 # Load the datasets iris
@@ -30,3 +33,73 @@ y = df[df.columns[-1]]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 print(X_train[:5])
+
+# Scaling
+
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+print(X_train[:5])
+
+# K-Nearest Neighbors
+
+k = 1
+
+classifier = KNeighborsClassifier(n_neighbors=k)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+result = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:")
+print(result)
+result1 = classification_report(y_test, y_pred)
+print("Classification Report:")
+print(result1)
+result2 = accuracy_score(y_test, y_pred)
+print("Accuracy:", result2)
+
+
+# Best K
+def knn_turning(k):
+    classifier = KNeighborsClassifier(n_neighbors=k)
+    classifier.fit(X_train, y_train)
+    y_pred = classifier.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    return accuracy
+
+
+print(knn_turning(1))
+print(knn_turning(5))
+
+knn_results = pd.DataFrame({"K": np.arange(1, len(X_train), 5)})
+print(knn_results["K"])
+
+knn_results["Accuracy"] = knn_results["K"].apply(knn_turning)
+print(knn_results)
+
+# Optimize weights
+
+
+def knn_tuning_uniform(k):
+    classifier = KNeighborsClassifier(n_neighbors=k, weights="uniform")
+    classifier.fit(X_train, y_train)
+    y_pred = classifier.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    return accuracy
+
+
+def knn_tuning_distance(k):
+    classifier = KNeighborsClassifier(n_neighbors=k, weights="distance")
+    classifier.fit(X_train, y_train)
+    y_pred = classifier.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    return accuracy
+
+
+knn_results["Uniform"] = knn_results["K"].apply(knn_tuning_uniform)
+knn_results["Distance"] = knn_results["K"].apply(knn_tuning_distance)
+
+print(knn_results)
