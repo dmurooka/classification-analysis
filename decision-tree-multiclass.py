6a20fc501b14edadc8739ddfbcaf048cc69dd9a3
Daishin Murooka <dmurooka@gmail.com>
2024-05-21 15:03:51 +0900

    Demonstrate decision tree multiclass

--- decision-tree-multiclass.py
+++ decision-tree-multiclass.py
@@ -4,3 +4,18 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+
+
+iris = datasets.load_iris()
+
+df = pd.DataFrame(
+    {
+        "Sepal length": iris.data[:, 0],
+        "Sepal width": iris.data[:, 1],
+        "Petal legnth": iris.data[:, 2],
+        "Petal width": iris.data[:, 3],
+        "Species": iris.target,
+    }
+)
+
+print(df.head())
