6e8bea419c0c1033ec8ff382edc6befc1289a1a7
Daishin Murooka <dmurooka@gmail.com>
2024-05-30 10:57:45 +0900

    Demonstrate svm skillsets

---
+++ svm-binary.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
+
+iris = datasets.load_iris()
+
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+df["species"] = iris.target
+df = df[df["species"] != 0]
+print(df.head())
+print(df.info())
+
+sns.relplot(data=df, x="sepal length (cm)", y="sepal width (cm)", hue="species")
+
+# Train-test split
+X = df[df.columns[:2]]
+y = df[df.columns[-1]]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+print(X_train[:5])
+
+# Train model
+model = svm.SVC(kernel="linear")
+classifier = model.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+print(accuracy_score(y_test, y_pred))
